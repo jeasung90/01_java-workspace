a5079abab885eded57eb57e1a71371eb85d5654d
jeasung90 <dybosn@naver.com>
2023-06-07 11:56:28 +0900

    2

---
+++ 05_Object/src/com/kh/chap04/_field/Run/FieldRun.java
@@ -0,0 +1,13 @@
+package com.kh.chap04._field.Run;
+
+import com.kh.chap04._field.model.vo.FieldTest1;
+
+public class FieldRun {
+
+	public static void main(String[] args) {
+
+		FieldTest1 f =new FieldTest1();
+		
+	}
+
+}
