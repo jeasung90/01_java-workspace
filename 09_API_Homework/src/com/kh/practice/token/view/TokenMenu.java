05b61ffd5fc40d2dea5edd8dc2fb40596a5e3da7
jeasung90 <dybosn@naver.com>
2023-06-13 17:26:21 +0900

    1

--- 09_API_Homework/src/com/kh/practice/token/view/TokenMenu.java
+++ 09_API_Homework/src/com/kh/practice/token/view/TokenMenu.java
@@ -1,5 +1,26 @@
 package com.kh.practice.token.view;
 
-public class TokenMenu {
+import java.util.Scanner;
 
+public class TokenMenu {
+	Scanner sc = new Scanner(System.in);
+
+	while (true) {
+        System.out.println("1. 지정 문자열");
+        System.out.println("2. 입력 문자열");
+        System.out.println("9. 프로그램 끝내기");
+        int num = sc.nextInt();
+        sc.nextLine();
+        switch (num) {
+        case 1:
+           tokenMenu();
+        case 2:
+           inputMenu();
+        case 9:
+           System.out.println("프로그램을 종료합니다");
+           break;
+        default:
+           System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+        }
+     }
 }
