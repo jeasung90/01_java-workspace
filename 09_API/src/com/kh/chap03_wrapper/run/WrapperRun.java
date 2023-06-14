0cb2db2d6cab055233848357fd239e9c277f1a88
jeasung90 <dybosn@naver.com>
2023-06-14 09:19:53 +0900

    1

---
+++ 09_API/src/com/kh/chap03_wrapper/run/WrapperRun.java
@@ -0,0 +1,29 @@
+package com.kh.chap03_wrapper.run;
+
+public class WrapperRun {
+	
+	/*
+	 *  * Wapper 클래스
+	 *  => 기본자료형을 객체로 포장해줄 수 있는 클래스가 래퍼클래스 이다.
+	 *  
+	 *  기본자료형 <----> Wrapper 클래스
+	 *  boolean		   boolean
+	 *  char		   Character ***
+	 *  byte		   byte
+	 *  short		   Short
+	 *  int			   Integer ***
+	 *  long		   Long
+	 *  float		   Float
+	 *  double		   Double
+	 *  
+	 *  => 기본자료형을 객체로 취급해야하는 경
+	 *  		- 메소드를 호출해야 될 
+	 *  
+	 */
+
+	public static void main(String[] args) {
+
+		System.out.println();
+	}
+
+}
