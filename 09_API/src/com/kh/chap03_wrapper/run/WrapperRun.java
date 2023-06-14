1a5cadf9bbc73c52f95570216e5e0bdeea3a2f48
jeasung90 <dybosn@naver.com>
2023-06-14 14:03:31 +0900

    1

--- 09_API/src/com/kh/chap03_wrapper/run/WrapperRun.java
+++ 09_API/src/com/kh/chap03_wrapper/run/WrapperRun.java
@@ -18,12 +18,92 @@ public class WrapperRun {
 	 *  
 	 *  => 기본자료형을 객체로 취급해야하는 경
 	 *  		- 메소드를 호출해야 될 
-	 *  
+	 *  		- 메소드의 매개변수로 기본자료형이 아닌 객체타입만이 요구될 때
+	 *  		- 다형성을 적용하고 싶을 
 	 */
 
+		// Boxing : 기본자료형 => Wrapper 클래스 자료
+	
 	public static void main(String[] args) {
+		int num1 = 10;
+		int num2 = 15;
+		
+		// System.out.println(num1.equals(num2)); // 주소값이라는 개념이 없어서 안
+		// equals => String  => 오버라이딩 : 실제값 빋교
+		// equals => 주소값 비교 
+		//System.out.println(num1 == num2);
+		
+		// 1. 객체 생성 구문을 통한 방법
+		Integer  i1 = new Integer(num1);
+		Integer  i2 = new Integer(num2);
+		
+		System.out.println(i1);
+		System.out.println(i2);
+		
+		System.out.println(i1==i2); // 이제는 주소값이 있어서 비교 가능
+		
+		System.out.println(i1.equals(i2));
+		System.out.println(i1.compareTo(i2)); // 두 값을 비교하여 앞쪽이 크면 1, 뒤쪽이 크면 -1, 같으면 0
+		// 변수.~~~ 을 쓰고싶으면 이렇게 객체화 시키는 박싱을 해야한다.
+		
+		// 2. 객체 생성 따위 하지 않고 곧바로 대입하는 방법 (AutoBoxing)
+		Integer i3 = num1;
+		System.out.println(i3);
+		
+		// 객체생성을 통해서 반드시 변환해야 하는 경우 => 문자열을 Integer 타입으로 변경해야 하는 경우
+		// Integer i4 = "123";
+		Integer i4 = new Integer("123"); // " " 사이에 반드시 숫자를 넣어야 함
+		System.out.println(i4);
+		
+		// UnBoxing : Wrapper 클래스 자료형 => 기본자료형
+		
+	
+		// 1. 해당 그  Wrapper 클래스에서 제공하는 xxxValue() 메소드를 통해
+		int num3 = i3.intValue(); // num3 = 10
+		int num4 = i4.intValue(); // num4 = 15
+		
+		
+		// 2. 메소드 따위 사용하지 않고 바로 대입하는 방법 (AutoUnBoxing)
+		int num5 = i1;
+		
+		// 이런거 왜배우나요?
+		// -> 웹 개발할때 값 입력하면 무조건 문자로 날아옴
+		System.out.println("=================");
 
-		System.out.println();
+		String str1 = "10";
+		String str2 = "15.3";
+		
+		
+		System.out.println(str1+str2); // 문자열 + 문자열 = 문자열
+		
+		// 1. String --> 기본 자료형
+		// 	"10"     --> 인트형 10
+		// 	"15.3"   --> 인트형 15.3
+		//   해당 Wrapper.parseXXX() 	사용
+		
+		int i = Integer.parseInt(str1);  // 문자열을 인트형 숫자로 변경해줌 테스트
+		double d = Double.parseDouble(str2);
+		
+		System.out.println(i+d);
+	
+	
+		// 2. 기본형 --> String
+		// 	  10   --> "10"
+		//	  15.3 --> "15.3"
+		
+		String strI = String.valueOf(i);
+		String strD = String.valueOf(d);
+		
+		System.out.println(strI);
+		System.out.println(strD);
+		
+		// 근데 이렇게는 많이 안씀
+		String test = i+""; // String => 다른자료형 + 문자열 => 문자
+		
+	
+	
+	
+	
 	}
 
 }
---
+++ 10_Exception/src/com/kh/excepticon/controller/A_UncheckdException.java
@@ -0,0 +1,154 @@
+package com.kh.excepticon.controller;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class A_UncheckdException {
+	
+	/*
+	 *   *  RuntimeException
+	 *  - 프로그램 실행시 발생되는 예외들
+	 *  
+	 *   *  RuntimeException의 후손들
+	 *  - IndexOutOfBoundsException : 부적절한 인덱스를 가지고 접근시 발생되는 예외
+	 *  - NullPointException : 참조변수가 null로 초기화된 상황에서 null에 접근했을때 발생되는 예
+	 *  - ArithmeticException : 나누기 연산시 0으로 나누려 할 때 발생되는 예외
+	 *  - ClassCastException : 허용할 수 없는 형변환이 진행될 때 발생되는 예
+	 *  - NegativeArraySizeException : 배열 할당 과정에서 배열의 크기를 음수로 지정하는 경우 발생되는 예외
+	 *  
+	 *  => RuntimeEception 관련된 예외는 충분히 예측가능한 상황이 있음
+	 *  애초에 예외 발생이 안되게끔 if문 조건처리 가능
+	 */
+	
+	Scanner sc = new Scanner(System.in);
+	
+	
+	public void method1() {
+		// ArithmeticException : 나누기 연산시 0으로 나누려 할 때 발생되는 예외
+		
+		System.out.print("첫번쨰 정수 : ");
+		int num1 = sc.nextInt();
+		
+		System.out.print("두번쨰 정수 : ");
+		int num2 = sc.nextInt();
+		
+		// int result = num1 / num2 ;
+		
+		
+		// 애초에 예외 자체가 발생이 안되게끔 if문으로 조건 검사 => 예외처리 아님!! 사전 방
+		/*
+		int result =0;
+		if(num2!=0) {
+			result=num1 / num2;
+		}
+		
+		System.out.println("계산결과 : "+result);
+		*/
+		
+		// 	예외처리 : 예외가 "발생했을때" 실행할 내용을 미리 작성해놓는 방법
+		
+		/*
+		 *    [ 표현법 ]
+		 *  try catch 방법
+		 *  try{
+		 *  	예외가 발생될 수 있는 구문;
+		 *  catch(발생될 예외 클래스 매개변수) {
+		 *  	해당 예외가 발생됬을 경우 실행할 구문;
+		 *  }
+		 */
+		
+	try {
+		int result = num1 / num2;
+		System.out.println("result : "+result);
+	}catch(ArithmeticException e) {
+		System.out.println("0으로 나눌 수 없습니다.");
+		e.printStackTrace();	// 강제로 오류난 이력을 보고자 할 
+	}
+		
+		System.out.println("프로그램을 종료합니다.");
+		
+	}
+	
+	public void method2() {
+		// NegativeArraySizeEception : 배열 할당 과정에서 배열의 크기를 음수로 지정하는 경우 발생되는 예외
+		// ArrauIndexOutOfBoundsExeption : 배열에 부적절한 인덱스를 가지고 접근시 발행되는 예외
+		
+		System.out.println("배열의 크기 : ");		
+		int size = sc.nextInt();
+				
+		//int[] arr = new int[size];
+		/* if문으로 미리 방지한거지 예외처리는 아니다.
+		if(size>=101) {
+			System.out.println("100번 인덱스 값 : "+arr[100]);
+		}
+		*/
+		
+		//System.out.println("100번 인덱 값 : "+arr[100]);
+		
+		try {
+			int[] arr = new int[size];
+		System.out.print("100번 인덱스 값 : "+arr[100]);
+		}catch(NegativeArraySizeException e) {
+			System.out.println("배열의 크기로 음수를 제시할 수 없습니다.");
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("부적절한 인덱스로 접근했습니다.");
+		}
+		
+		// 다중 catch 블럭 작성 가능
+		System.out.println("프로그램을 종료 합니다.");
+		
+	}
+	
+	public void method3() {
+		System.out.print("배열의 크기 : ");
+		
+		/*
+		try {
+			int size = sc.nextInt();
+			int[] arr = new int[size];
+		System.out.print("100번 인덱스 값 : "+arr[100]);
+		}catch(NegativeArraySizeException e) {
+			System.out.println("배열의 크기로 음수를 제시할 수 없습니다.");
+		}catch(ArrayIndexOutOfBoundsException e) {
+			System.out.println("부적절한 인덱스로 접근했습니다.");
+		}catch(InputMismatchException e) { // import => java.util
+			System.out.println("정수로만 입력해야 합니다.");
+		}
+		*/
+		
+
+		try {
+			int size = sc.nextInt();
+			int[] arr = new int[size];
+			System.out.print("100번 인덱스 값 : "+arr[100]);
+		}catch(InputMismatchException e ) { // 부모에 의해서 실행이 안될 수 있으니 순서가 중요함!!
+			System.out.println("정수가 아닌 값을 입력");
+		}catch(RuntimeException e) { // 다형성 적용해서 부모타입 예외클래스 작성 가능
+									 // 모든 자식 예외 발생시 다 받아서 처리할 수 있음 // 정확한 원인을 찾을 수 없음
+			System.out.println("예외가 발생되긴함.. 배열의 크기가 잘못됐거나, 부적절한 인덱스를 제시됐던가, 정수 입력을 안했던가...");
+		}
+		
+		// 부모예외클래스와 자식예외클래스를 가지고 catch 블럭을 기술하고자 할 때는
+		// 자식예외클래스 catch	블럭이 위에 있어야 함 (아니면 unreachable catch block 오류 뜸)
+		
+		/*
+		 *  RuntimeExection 관련애들은 UnCheckedException (예외처리구문이 필수는 아니다.)
+		 *  if문 : 예외가 발생되기도 전에 소스코드로 핸들링 하는거임 ( 예외처리 구문은 아님, 사전 예방)
+		 *  try - catch문 : 예외가 "발샐했을 경우" 처리해주는 구문을 작성해두는것  (예외처리 구문임)
+		 *  
+		 *  예측가능한 상황은 if문으로 조건검사를 해결하는게 권장사항임!1
+		 *  부득이하게 조건문으로 핸들링이 안되는 구문은 예외처리 구문으로 작성해둘 것!
+		 *  
+		 */
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
